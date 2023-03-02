b475e5ceded95a3818d739544cba3461b1a2bbc3
SWAPAN MANDI <swapanmandi5@gmail.com>
2023-03-02 18:14:18 +0530

    update the features

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './Laoyouts/Navbar';
 import Footer from './Laoyouts/footer';
+import LeftAside from './Laoyouts/leftAside';
+import RightAside from './Laoyouts/rightAside';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar/>
+    <LeftAside/>
+    <RightAside/>
     <App />
     <Footer/>
   </React.StrictMode>

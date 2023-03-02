c88971bbda0aa1906a0051cbb1de00b6f61c3807
SWAPAN MANDI <swapanmandi5@gmail.com>
2023-03-03 00:43:41 +0530

    create table contents

--- src/Laoyouts/leftAside.js
+++ src/Laoyouts/leftAside.js
@@ -4,7 +4,16 @@ import './leftAside.css';
 function LeftAside(){
     return(
         <div className="laside">
-            <h1>left aside ad</h1>
+            <h4>contents</h4>
+            <div className="table-div">
+                <table>
+                    <ol>
+                        <li className="list">The Night Train at Deoli” by Ruskin Bond</li>
+                        <li className="list">There Will Come Soft Rains” by Ray Bradbury</li>
+                        <li className="list">Orientation” by Daniel Orozco</li>
+                    </ol>
+                </table>
+            </div>
         </div>
     )
 

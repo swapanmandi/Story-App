3cd566b191611151c8534a44d54eae65d3a7439e
SWAPAN MANDI <swapanmandi5@gmail.com>
2023-03-02 14:10:02 +0530

    add footer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './Laoyouts/Navbar';
+import Footer from './Laoyouts/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar/>
     <App />
+    <Footer/>
   </React.StrictMode>
 );
 

e672e71eadeee9456923a39dd8cd6c1c3a2f8062
SWAPAN MANDI <swapanmandi5@gmail.com>
2023-03-02 14:14:10 +0530

    update footer

---
+++ src/Laoyouts/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './footer.css';
+
+function Footer(){
+    return(
+      <div className="footer">
+        <a>Copyright</a>
+      </div>
+
+    )
+}
+
+
+
+
+export default Footer;
